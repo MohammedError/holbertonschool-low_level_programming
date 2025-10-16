92ff59a443f4e5d1b2bea5d780827d9871d94e4c
MohammedError <mohammed.secure@outlook.sa>
2025-10-16 10:02:08 +0000

    Add solution for 10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @a: the first number to be added
+ * @b: the second number to be added
+ *
+ * Return: the sum of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
