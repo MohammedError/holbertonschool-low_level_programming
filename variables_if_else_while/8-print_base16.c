5b4e57efd2ff1bf6e7686a19e647a41cbe99e3d9
MohammedError <mohammed.secure@outlook.sa>
2025-10-16 09:10:39 +0000

    Add solution for 8-print_base16.c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all the numbers of base 16 in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit;
+	char letter;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+		putchar(digit + '0');
+	}
+
+	for (letter = 'a'; letter <= 'f'; letter++)
+	{
+		putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
