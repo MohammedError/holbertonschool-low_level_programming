82ca56724b6b14451af582e7da0b3ba1106d685a
MohammedError <mohammed.secure@outlook.sa>
2025-11-03 21:22:15 +0000

    Add solutions for C - More malloc, free (Tasks 0-3)

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array, using malloc
+ * @nmemb: Number of elements
+ * @size: Size of each element
+ *
+ * Return: Pointer to allocated memory, all set to 0.
+ * NULL if nmemb or size is 0, or if malloc fails.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+	unsigned int i, total_size;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	total_size = nmemb * size;
+	ptr = malloc(total_size);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < total_size; i++)
+		ptr[i] = 0;
+
+	return ((void *)ptr);
+}
