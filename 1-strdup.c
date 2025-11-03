ed6419089465cc10784d398369738cc33a0bfa5f
MohammedError <mohammed.secure@outlook.sa>
2025-11-03 20:51:12 +0000

    Add _strdup function for malloc_free project

--- 1-strdup.c
+++ 1-strdup.c
@@ -17,9 +17,8 @@ char *_strdup(char *str)
 	if (copy == NULL)
 		return (NULL);
 
-	for (i = 0; i < len; i++)
+	for (i = 0; i <= len; i++)
 		copy[i] = str[i];
-	copy[i] = '\0';
 
 	return (copy);
 }
