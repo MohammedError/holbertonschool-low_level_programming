cd5cdc17579258ebcd4662ec488a34bbcf5a159c
MohammedError <mohammed.secure@outlook.sa>
2025-11-03 20:49:47 +0000

    Add _strdup function for malloc_free project

---
+++ 1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+
+char *_strdup(char *str)
+{
+	char *copy;
+	int i, len;
+
+	if (str == NULL)
+		return (NULL);
+
+	len = 0;
+	while (str[len])
+		len++;
+
+	copy = malloc(sizeof(char) * (len + 1));
+	if (copy == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+		copy[i] = str[i];
+	copy[i] = '\0';
+
+	return (copy);
+}
