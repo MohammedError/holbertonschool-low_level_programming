00807b383742bff966fa13ac761df1e6dfe86453
MohammedError <mohammed.secure@outlook.sa>
2025-11-11 13:27:32 +0000

    Add solution for C - Variadic functions (Task 0)

---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdarg.h> /* For va_list, va_start, va_arg, va_end */
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+
+#endif /* VARIADIC_FUNCTIONS_H */
