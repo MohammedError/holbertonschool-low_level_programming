9cdf4f4c7c9fdbedc28856b1ca0269554fa24ad7
MohammedError <mohammed.secure@outlook.sa>
2025-11-25 12:02:25 +0000

    Task 11: Fix style and logic

--- doubly_linked_lists/103-keygen.c
+++ doubly_linked_lists/103-keygen.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include <time.h>
 
+/**
+ * main - Generates a key for the crackme5 executable.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
+ */
 int main(int argc, char *argv[])
 {
 	char password[100];
@@ -26,7 +33,9 @@ int main(int argc, char *argv[])
 		sum += n;
 		i++;
 	}
-	password[i] = 2772 - sum;
+
+	n = 2772 - sum;
+	password[i] = n;
 	password[i + 1] = '\0';
 
 	printf("%s", password);
