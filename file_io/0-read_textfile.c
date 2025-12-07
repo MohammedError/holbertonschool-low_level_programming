970eb1fcad1367779a4faff2b5dbfb907f002533
MohammedError <mohammed.secure@outlook.sa>
2025-12-07 07:42:17 +0000

    Task 0: Fix Betty doc

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,40 +1,37 @@
 #include "main.h"
 
+/**
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the function should read and print.
+ *
+ * Return: If the function fails or filename is NULL - 0.
+ * O/w - the actual number of bytes the function can read and print.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t len_r, len_w;
+	ssize_t op, rd, wr;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
-
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
-	{
-		close(fd);
 		return (0);
-	}
 
-	len_r = read(fd, buffer, letters);
-	if (len_r == -1)
+	op = open(filename, O_RDONLY);
+	rd = read(op, buffer, letters);
+	wr = write(STDOUT_FILENO, buffer, rd);
+
+	if (op == -1 || rd == -1 || wr == -1 || wr != rd)
 	{
 		free(buffer);
-		close(fd);
 		return (0);
 	}
 
-	len_w = write(STDOUT_FILENO, buffer, len_r);
-
 	free(buffer);
-	close(fd);
+	close(op);
 
-	if (len_w != len_r)
-		return (0);
-
-	return (len_w);
+	return (wr);
 }
